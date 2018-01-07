d140c01e173b36806d0c5683e4bdf41f184cb81d
kanitsharma <kanitshrm09@gmail.com>
2018-01-08 02:26:29 +0530

    Added sass and HRM support

--- src/index.js
+++ src/index.js
@@ -7,12 +7,22 @@ import Home from './connectors/home/main'
 import './base.css'
 import registerServiceWorker from './registerServiceWorker'
 
-
 ReactDOM.render(
   <Provider store={Store}>
     <Home />
-  </Provider>
-  , document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 )
+
+if (module.hot) {
+  module.hot.accept('./connectors/home/main', () => {
+    ReactDOM.render(
+      <Provider store={Store}>
+        <Home />
+      </Provider>,
+      document.getElementById('root')
+    )
+  })
+}
 
 registerServiceWorker()
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware } from 'redux'
 import { logger } from './logger'
-import RootReducer from './rootReducer'
+import rootReducer from './rootReducer'
 
 const middleware = applyMiddleware(logger)
-const store = createStore(RootReducer, middleware)
+const store = createStore(rootReducer, middleware)
+
+//enabling HMR
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    module.hot.accept('./rootReducer', () => {
+      store.replaceReducer(rootReducer)
+    })
+  }
+}
 
 export default store
